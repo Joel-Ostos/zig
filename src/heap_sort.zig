92c3a700b83746ff0ce22cbd7219ec1dcbcea386
Joel Ostos <joelostoscastro@gmail.com>
2023-10-30 23:30:50 -0500

    heap sort algorithm

---
+++ src/heap_sort.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+
+pub fn swap(a: *i32, b: *i32) void{
+    var tmp: i32 = a.*;
+    a.* = b.*;
+    b.* = tmp;
+} 
+
+pub fn max_heapify(arr: []i32, N:usize ,i: usize) void{
+    var a: usize = (2*i)+1;
+    var b: usize = (2*i)+2;
+    std.log.info("i = {} a = {} b = {}", .{i,a,b});
+    var largest: usize = i;
+    if (a < N and arr[a] > arr[largest]) largest = a;
+    if (b < N and arr[b] > arr[largest]) largest = b;
+    if (largest != i){
+        swap(&arr[i], &arr[largest]);
+        max_heapify(arr, N,largest);
+    }
+}
+
+pub fn heap_sort(array: []i32) void{
+    var pivot = @divFloor(array.len, 2);
+    while (pivot >= 0) : (pivot -= 1){
+        max_heapify(array, array.len, pivot);
+        if (pivot == 0){
+            break;
+        }
+    }
+
+    var a: usize = array.len-1;
+    while (a >= 0) : (a-=1) {
+        swap(&array[0], &array[a]);
+        max_heapify(array, a, 0);
+        if (a == 0){
+            break;
+        }
+    }
+}
+
+pub fn main() void {
+    var array = [_]i32{13,4,1,26,7,8,9};
+    heap_sort(&array);
+    for (array) |i|{
+        std.log.info("{}", .{i});
+    }
+}
+    
--- src/linkedlist.zig
+++ src/linkedlist.zig
@@ -49,12 +49,12 @@ fn LinkedList(comptime T: type) type {
         }
 
         pub fn reverse(self: *Self) void {
-            if (self.size < 1){
+            if (self.size < 1) {
                 std.log.info("Empty list", .{});
                 return;
             }
             var current = self.head;
-            while (current) |curr| : (current = curr.next){
+            while (current) |curr| : (current = curr.next) {
                 var tmp = curr.next;
                 curr.next = curr.prev;
                 curr.prev = tmp;
@@ -65,10 +65,9 @@ fn LinkedList(comptime T: type) type {
             return;
         }
 
-        // TODO implementar función para eliminar algún elemento por su indice o por su valor
         pub fn del(curr: *Node, allocator: std.mem.Allocator) ?T {
-            if (curr.prev) |a|{
-                if (curr.next) |b|{
+            if (curr.prev) |a| {
+                if (curr.next) |b| {
                     b.prev = a;
                     a.next = b;
                 }
@@ -77,54 +76,78 @@ fn LinkedList(comptime T: type) type {
             return curr.data;
         }
 
-        pub fn deleteByIndex(self: *Self,index: usize) ?T {
-            if (index == 0){
-                if (self.tail) |tail|{
+        pub fn deleteByIndex(self: *Self, index: usize) ?T {
+            if (index == 0) {
+                if (self.tail) |tail| {
                     self.tail = tail.next;
                     self.size -= 1;
                     defer self.allocator.destroy(tail);
                     return tail.data;
                 }
             }
-            if (index == self.size-1){
-                if (self.head) |head|{
+            if (index == self.size - 1) {
+                if (self.head) |head| {
                     self.head = head.prev;
                     self.size -= 1;
                     defer self.allocator.destroy(head);
                     return head.data;
                 }
             }
-            if (index < @divFloor(self.size,2)){
+            if (index < @divFloor(self.size, 2)) {
                 var current = self.tail;
                 var con: usize = 0;
                 while (current) |curr| : (current = curr.next) {
-                    if (con == index){
+                    if (con == index) {
                         var check = del(curr, self.allocator);
-                        if (check != null) return check;
+
+                        if (check) |c| {
+                            self.size -= 1;
+                            return c;
+                        }
                     }
-                    con+=1;
+                    con += 1;
                 }
-            } else if (index < self.size){
+            } else if (index < self.size) {
                 var current = self.head;
-                var con: usize = self.size-1;
+                var con: usize = self.size - 1;
                 while (current) |curr| : (current = curr.prev) {
-                    if (con == index){
+                    if (con == index) {
                         var check = del(curr, self.allocator);
-                        if (check != null) return check;
+                        if (check) |c| {
+                            self.size -= 1;
+                            return c;
+                        }
                     }
-                    con-=1;
+                    con -= 1;
                 }
             }
             std.log.info("Out of bounds", .{});
             return null;
         }
 
-        // TODO implementar la eliminación de la cabeza y de la cola XD
+        // TODO Hacer una revisión general de la implementación de esta función.
         pub fn deleteByName(self: *Self, data: T) void {
             var curr = self.head;
-            while (curr) |node| : (curr = node.prev){
-                if (node.data == data){
+            while (curr) |node| : (curr = node.prev) {
+                if (node.data == data and node != self.head and node != self.tail) {
                     _ = del(node, self.allocator);
+                    return;
+                }
+            }
+            if (self.head != null and self.head.?.data == data) {
+                if (self.head) |head| {
+                    self.head = head.prev;
+                    self.size -= 1;
+                    self.head.?.next = null;
+                    self.allocator.destroy(head);
+                    return;
+                }
+            }
+            if (self.tail != null and self.tail.?.data == data) {
+                if (self.tail) |tail| {
+                    self.tail = tail.next;
+                    self.size -= 1;
+                    self.allocator.destroy(tail);
                     return;
                 }
             }
@@ -134,13 +157,13 @@ fn LinkedList(comptime T: type) type {
         pub fn quickSort() void {}
 
         // TODO implementar binary search para la busqueda
-        pub fn binarySearch() void{}
-        
+        pub fn binarySearch() void {}
+
         pub fn find(self: *Self, value: T) ?T {
             var current = self.head;
 
-            while (current != null){
-                if (current.?.data == value){
+            while (current != null) {
+                if (current.?.data == value) {
                     return value;
                 }
                 current = current.?.prev;
@@ -150,7 +173,7 @@ fn LinkedList(comptime T: type) type {
 
         pub fn print(self: *Self) void {
             var curr = self.tail;
-            while (curr) |node| : (curr = node.next){
+            while (curr) |node| : (curr = node.next) {
                 std.log.info("-> {d}", .{node.data});
             }
         }
@@ -160,6 +183,18 @@ fn LinkedList(comptime T: type) type {
 const prueba = LinkedList(i32);
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    defer arena.deinit();
+    var a = prueba.init(allocator);
+
+    for (0..10) |i| {
+        try a.pushFront(@intCast(i));
+    }
+
+    for (0..10) |i| {
+        std.log.info("{} -> {?}", .{ i, a.deleteByIndex(5) });
+    }
 }
 
 test "pushing some items" {
@@ -176,17 +211,16 @@ test "pushing some items" {
 
 test "delete by index" {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
     defer arena.deinit();
-    var a = prueba.init(allocator);
+    const allocator = arena.allocator();
+    const constructor = LinkedList(i32);
+    var a = constructor.init(allocator);
 
     for (0..10) |i| {
-        try a.pushFront(@intCast(i)); 
+        try a.pushFront(@intCast(i));
     }
 
-    for (0..10) |i|{
-        std.log.info("{} -> {?}", .{i,a.deleteByIndex(0)});
-    }
+    std.log.info("-> {?}", .{a.deleteByIndex(0)});
 }
 
 test "Reverse list" {
@@ -195,7 +229,7 @@ test "Reverse list" {
     defer arena.deinit();
     var a = prueba.init(allocator);
     for (0..10) |i| {
-        try a.pushFront(@intCast(i)); 
+        try a.pushFront(@intCast(i));
     }
     a.reverse();
 }
@@ -203,14 +237,14 @@ test "Reverse list" {
 test "Delete element by name" {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = arena.allocator();
-    defer arena.deinit();
+    //defer arena.deinit();
     var a = prueba.init(allocator);
 
     for (0..10) |i| {
-        try a.pushFront(@intCast(i)); 
+        try a.pushFront(@intCast(i));
     }
 
-    for (0..10) |i|{
-        std.log.info("{} -> {?}", .{i,a.deleteByIndex(5)});
+    for (0..10) |i| {
+        std.log.info("{} -> {?}", .{ i, a.deleteByIndex(5) });
     }
 }
